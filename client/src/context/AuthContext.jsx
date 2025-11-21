c6d3a4f813bf249ff4a8733039faaf7f9576dca6
castilloBrayan <ordonezcastillo20@gmail.com>
2025-11-21 00:30:45 -0600

    Componente Navbar y Cierre de Sesion

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -6,16 +6,32 @@ export const AuthProvider = ({ children }) => {
     // Estado para simular si el usuario está logueado y qué rol tiene
     const [user, setUser] = useState({
         isLoggedIn: false,
-        rol: 'Visitante'
+        rol: 'Visitante',
+        nombre: null,
+        id: null,
+        foto_url: null
     })
 
     // Por ahora, simulo una función de login
-    const login = (role = 'Instructor') => {
-        setUser({ isLoggedIn: true, rol: role })
+    const login = (userData) => { 
+        // Cuando el login es exitoso, guardar los datos que vienen del backend
+        setUser({ 
+            isLoggedIn: true, 
+            rol: userData.rol,
+            nombre: userData.nombre,
+            id: userData.id,
+            foto_url: userData.foto_url
+        })
     }
-    
+
     const logout = () => {
-        setUser({ isLoggedIn: false, rol: 'Visitante' })
+        setUser({ 
+            isLoggedIn: false, 
+            rol: 'Visitante',
+            nombre: null, // Limpiar nombre
+            id: null, // Limpiar ID
+            foto_url: null // Limpiar foto
+        })
     }
 
     return (
