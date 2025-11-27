7f3eb9219e8ccdb95d788ffd89826c5ecd793610
castilloBrayan <ordonezcastillo20@gmail.com>
2025-11-27 00:16:03 -0600

    Endpoint: Checkout (Procesar Compra)

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { authMiddleware } from '../middlewares/auth.middleware.js'
-import { addCourseToCart, getCart, removeCourseFromCart } from '../controllers/cart.controller.js'
+import { addCourseToCart, getCart, removeCourseFromCart, checkout } from '../controllers/cart.controller.js'
 
 const router = Router()
 
@@ -12,5 +12,8 @@ router.get('/', authMiddleware, getCart)
 
 // Ruta para eliminar curso del carrito (S2-CART-027)
 router.delete('/:id', authMiddleware, removeCourseFromCart) // El :id sería el ID del detalle_orden
+
+// Ruta para procesar el checkout (S2-CART-028)
+router.post('/checkout', authMiddleware, checkout)
 
 export default router
