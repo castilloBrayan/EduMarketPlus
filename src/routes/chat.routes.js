44547816b7b4bff7c52667a202fd534620c0484a
castilloBrayan <ordonezcastillo20@gmail.com>
2025-12-04 23:55:03 -0600

    Implementación de getChatHistory en chat.controller.js

--- src/routes/chat.routes.js
+++ src/routes/chat.routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express'
 import { authMiddleware } from '../middlewares/auth.middleware.js'
 import { chatAccessMiddleware } from '../middlewares/chat.middleware.js'
-import { joinPrivateChat, getConversationsList } from '../controllers/chat.controller.js'
+import { joinPrivateChat, getConversationsList, getChatHistory } from '../controllers/chat.controller.js'
 
 const router = Router()
 
 // Ruta para obtener la Lista de Conversaciones (S3-CHAT-048)
 router.get('/conversations', authMiddleware, getConversationsList)
+
+// Ruta para obtener el historial de Chat con Paginación (S3-CHAT-046/047)
+router.get('/history/:room_id', authMiddleware, getChatHistory)
 
 // Ruta HTTP para Unirse a Chat Privado o Soporte
 router.get(
